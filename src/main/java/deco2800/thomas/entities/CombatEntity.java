177dbe6ec2239ca7203356a9aa2ab71d9dd257a5
Chris Barnett <c.barnett@uqconnect.edu.au>
2020-08-25 18:19:07 +1000

    Created CombatEntity interface stub.

---
+++ src/main/java/deco2800/thomas/entities/CombatEntity.java
@@ -0,0 +1,11 @@
+package deco2800.thomas.entities;
+
+/**
+ * Implemented by entity classes that engage in combat. Requires
+ * the entity to have both health, and a bounding box.
+ */
+public interface CombatEntity {
+    // Health interface / class (WHY WOULD IT BE AN INTERFACE?!)
+
+    // Bounding box
+}
