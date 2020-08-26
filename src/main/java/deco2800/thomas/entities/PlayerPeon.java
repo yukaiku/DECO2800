4bf1be9ced1947124ac16466539bc6469893f15f
yukaiku <yukai.ku@uq.net.au>
2020-08-27 00:17:09 +1000

    added functions for quest tracking in PlayerPeon

--- src/main/java/deco2800/thomas/entities/PlayerPeon.java
+++ src/main/java/deco2800/thomas/entities/PlayerPeon.java
@@ -10,12 +10,49 @@ import deco2800.thomas.util.WorldUtil;
 
 public class PlayerPeon extends Peon implements TouchDownObserver {
 
+    /*Variable for orb count*/
+    int orbCount = 0;
+
     public PlayerPeon(float row, float col, float speed) {
         super(row, col, speed);
         this.setObjectName("playerPeon");
         GameManager.getManagerFromInstance(InputManager.class).addTouchDownListener(this);
     }
 
+
+    /**
+     *
+     * Quest Tracker function that tracks the orbs the user currently has
+     *
+     * @return orbCount the number of orbs the user currently has
+     */
+    public int questTracker(){
+        return orbCount;
+    }
+
+    /**
+     *
+     * Resets the number of orb user has
+     * Notes:
+     * To be used on when a new game is run or upon death
+     *
+     */
+    public void resetQuest(){
+        orbCount = 0;
+    }
+
+    /**
+     *
+     * Increase the number of orbs the user has
+     * and run the questTracker function to update progress
+     * Notes:
+     * To be used on when player picks up an orb
+     *
+     */
+    public void increaseOrbs(){
+        orbCount += 1;
+        questTracker();
+    }
 
 	@Override
     public void onTick(long i) {
