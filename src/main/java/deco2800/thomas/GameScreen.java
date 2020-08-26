12eb045aec518c3327142ca6e0eeae75c57d1465
Jay Huynh <thaiduong.huynh@uqconnect.edu.au>
2020-08-26 21:46:57 +1000

    Create zoomable camera function

--- src/main/java/deco2800/thomas/GameScreen.java
+++ src/main/java/deco2800/thomas/GameScreen.java
@@ -115,12 +115,11 @@ public class GameScreen implements Screen, KeyDownObserver {
 	public void render(float delta) {
 		handleRenderables();
 		
-//		moveCamera();
+		CameraUtil.zoomableCamera(camera, Input.Keys.MINUS, Input.Keys.EQUALS, delta);
 		CameraUtil.lockCameraOnTarget(camera, GameManager.get().getWorld().getPlayerEntity());
 			
 		cameraDebug.position.set(camera.position);
 		cameraDebug.update();
-//		camera.update();
 
 		SpriteBatch batchDebug = new SpriteBatch();
 		batchDebug.setProjectionMatrix(cameraDebug.combined);
@@ -244,47 +243,5 @@ public class GameScreen implements Screen, KeyDownObserver {
 			// Load the world to the DB
 			DatabaseManager.loadWorld(null);
 		}
-	}
-	
-	public void moveCamera() {
-	//timmeh to fix hack.  // fps is not updated cycle by cycle
-		float normilisedGameSpeed = (60.0f/Gdx.graphics.getFramesPerSecond());
-				
-		int goFastSpeed = (int) (5 * normilisedGameSpeed *camera.zoom);
-		
-		if (!camera.isPotate()) {
-			
-			if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-				goFastSpeed *= goFastSpeed * goFastSpeed;
-			}
-			
-			if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-				camera.translate(-goFastSpeed, 0, 0);
-			}
-	
-			if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-				camera.translate(goFastSpeed, 0, 0);
-			}
-	
-			if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-				camera.translate(0, -goFastSpeed, 0);
-			}
-	
-			if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-				camera.translate(0, goFastSpeed, 0);
-			}
-			
-			if (Gdx.input.isKeyPressed(Input.Keys.EQUALS)) {
-				camera.zoom *=1-0.01*normilisedGameSpeed;
-				if (camera.zoom < 0.5) {
-					camera.zoom = 0.5f;
-				}
-			}
-			
-			if (Gdx.input.isKeyPressed(Input.Keys.MINUS)) {
-				camera.zoom *=1+0.01*normilisedGameSpeed;
-			}
-		}
-		
 	}
 }
