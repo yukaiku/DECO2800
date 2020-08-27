1a8fe33cab75886ad714451f842d77f478d44514
Brayden Phillips <brayden@Braydens-MacBook-Pro.local>
2020-08-27 21:57:29 +1000

    migrated to a list over a map.

---
+++ src/main/java/deco2800/thomas/util/SpawnList.java
@@ -0,0 +1,6 @@
+package deco2800.thomas.util;
+
+import java.util.ArrayList;
+
+public class SpawnList<T> extends ArrayList<T> {
+}
--- src/main/java/deco2800/thomas/util/SpawnMap.java
+++
@@ -1,6 +0,0 @@
-package deco2800.thomas.util;
-
-import java.util.HashMap;
-
-public class SpawnMap<T> extends HashMap<T, SquareVector> {
-}
