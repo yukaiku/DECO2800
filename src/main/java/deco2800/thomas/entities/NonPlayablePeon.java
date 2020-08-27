6da31be2595da1b3086380dc97e17047f0dca61c
Brayden Phillips <brayden@Braydens-MacBook-Pro.local>
2020-08-27 13:36:17 +1000

    Added equality checks

--- src/main/java/deco2800/thomas/entities/NonPlayablePeon.java
+++ src/main/java/deco2800/thomas/entities/NonPlayablePeon.java
@@ -1,8 +1,17 @@
 package deco2800.thomas.entities;
 
+import java.util.Objects;
+
 public class NonPlayablePeon extends Peon implements Interactable {
 
     private PlayerPeon player;
+    private NonPlayablePeonType type;
+    private String name;
+
+    public NonPlayablePeon(NonPlayablePeonType type, String name) {
+        this.type = type;
+        this.name = name;
+    }
 
     @Override
     public void interact() {
@@ -11,5 +20,21 @@ public class NonPlayablePeon extends Peon implements Interactable {
 
     public void setPlayer(PlayerPeon player) {
         this.player = player;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NonPlayablePeon)) return false;
+        if (!super.equals(o)) return false;
+        NonPlayablePeon that = (NonPlayablePeon) o;
+        return Objects.equals(player, that.player) &&
+                type == that.type &&
+                name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), player, type, name);
     }
 }
