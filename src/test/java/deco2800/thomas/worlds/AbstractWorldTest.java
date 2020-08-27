78b2e91a83a150e53efb9c5e39a895f03f68d6b4
Chris Barnett <c.barnett@uqconnect.edu.au>
2020-08-27 19:13:32 +1000

    Added new test to AbstractWorldTest that checks that colliding entities are returned in a list correctly.

--- src/test/java/deco2800/thomas/worlds/AbstractWorldTest.java
+++ src/test/java/deco2800/thomas/worlds/AbstractWorldTest.java
@@ -54,4 +54,31 @@ public class AbstractWorldTest {
         // Should be an empty list
         assertEquals(0, collidingEntities.size());
     }
+
+    /**
+     * Tests that a list of entities is return when entities are
+     * found within bounds.
+     */
+    @Test
+    public void getEntityInBoundsTestMany() {
+        // Add at least 2 dummy entities to a test world
+        TestAbstractWorld world = new TestAbstractWorld();
+        TestAbstractEntity entityA = new TestAbstractEntity();
+        entityA.setCol(20);
+        entityA.setRow(20);
+        entityA.setBounds(new BoundingBox(new SquareVector(20, 20), 10, 10));
+        world.addEntity(entityA);
+        TestAbstractEntity entityB = new TestAbstractEntity();
+        entityB.setCol(40);
+        entityB.setRow(40);
+        entityB.setBounds(new BoundingBox(new SquareVector(40, 40), 10, 10));
+        world.addEntity(entityB);
+
+        // Get list of all entities in bounds
+        BoundingBox bounds = new BoundingBox(new SquareVector(0, 0), 50, 50);
+        List<AbstractEntity> collidingEntities = world.getEntitiesInBounds(bounds);
+        assertEquals(2, collidingEntities.size());
+        assertTrue(collidingEntities.contains(entityA));
+        assertTrue(collidingEntities.contains(entityB));
+    }
 }
