278ef496c67095816af3d3e5af3e9b884bc2365b
Wilson <s4528577@student.uq.edu.au>
2020-08-28 14:45:14 +1000

    Fix minion

--- src/main/java/deco2800/thomas/entities/Minion.java
+++ src/main/java/deco2800/thomas/entities/Minion.java
@@ -1,4 +1,9 @@
 package deco2800.thomas.entities;
 
-public class Minions {
+/**
+ * A class that defines a subcategory of enemy called a Minion.
+ * Features of the minion class will generally include a smaller size
+ * and the capability to be spawned by other enemies.
+ */
+public class Minion extends EnemyPeon {
 }
