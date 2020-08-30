d3ae9cea0d572d6a8f44f1d688e1f06ff51c85ae
ArthurM99115 <arthur.mitchell4@gmail.com>
2020-08-30 10:12:27 +1000

    First commit for Volcano Task

---
+++ src/main/java/deco2800/thomas/worlds/VolcanoWorld.java
@@ -0,0 +1,41 @@
+package deco2800.thomas.worlds;
+
+public class VolcanoWorld extends AbstractWorld{
+
+    public VolcanoWorld() {
+        super();
+    }
+
+    /**
+     * Generates the tiles for the world
+     */
+    protected void generateTiles() {
+
+    }
+
+    public void onTick(){
+
+    }
+
+    public void addCrater(){
+
+    }
+
+    public void addRuins(){
+
+    }
+
+    public void addLavaPool(){
+
+    }
+
+    public void addBones(){
+
+    }
+
+    public void volcanoEvent(){
+
+    }
+
+
+}
