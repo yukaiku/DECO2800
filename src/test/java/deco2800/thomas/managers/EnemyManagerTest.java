5851c95144d32678212afb368e8fa515fffecc88
Jay Huynh <thaiduong.huynh@uqconnect.edu.au>
2020-08-31 15:18:31 +1000

    Fix the build in EnemyManageTest

--- src/test/java/deco2800/thomas/managers/EnemyManagerTest.java
+++ src/test/java/deco2800/thomas/managers/EnemyManagerTest.java
@@ -1,5 +1,6 @@
 package deco2800.thomas.managers;
 
+import deco2800.thomas.BaseGDXTest;
 import deco2800.thomas.entities.enemies.Orc;
 import deco2800.thomas.entities.PlayerPeon;
 import deco2800.thomas.worlds.TestWorld;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-public class EnemyManagerTest {
+public class EnemyManagerTest extends BaseGDXTest {
 
     @Test
     public void testEnemyManager() {
