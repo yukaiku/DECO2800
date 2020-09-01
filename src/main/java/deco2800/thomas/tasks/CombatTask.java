82da60efc4d8f1eff26426d2a7b8027da6f86dcd
finn-cooney <s4482633@student.uq.edu.au>
2020-09-01 16:31:50 +1000

    Updated to show when a combat task is complete and to attack Orcs

--- src/main/java/deco2800/thomas/tasks/CombatTask.java
+++ src/main/java/deco2800/thomas/tasks/CombatTask.java
@@ -2,9 +2,8 @@ package deco2800.thomas.tasks;
 
 import deco2800.thomas.entities.AbstractEntity;
 import deco2800.thomas.entities.CombatEntity;
-import deco2800.thomas.entities.PlayerPeon;
+import deco2800.thomas.entities.enemies.EnemyPeon;
 import deco2800.thomas.managers.GameManager;
-import deco2800.thomas.managers.TextureManager;
 import deco2800.thomas.util.SquareVector;
 import deco2800.thomas.worlds.AbstractWorld;
 
@@ -16,6 +15,7 @@ public class CombatTask extends AbstractTask{
     private SquareVector position;
     private AbstractWorld world;
     private boolean taskAlive = true;
+    private boolean taskComplete = false;
 
     public CombatTask(CombatEntity entity) {
         super(entity);
@@ -26,7 +26,7 @@ public class CombatTask extends AbstractTask{
 
     @Override
     public boolean isComplete() {
-        return false;
+        return taskComplete;
     }
 
     @Override
@@ -37,6 +37,7 @@ public class CombatTask extends AbstractTask{
     @Override
     public void onTick(long tick) {
         List<AbstractEntity> collidingEntities = world.getEntitiesInBounds(entity.getBounds());
+        /*
         System.out.printf("PlayerPeon Width: %d\n", GameManager.getManagerFromInstance(TextureManager.class).getTexture("spacman_ded").getWidth());
         System.out.printf("PlayerPeon Height: %d\n", GameManager.getManagerFromInstance(TextureManager.class).getTexture("spacman_ded").getHeight());
         System.out.printf("Bottom: %f\n",entity.getBounds().getBottom());
@@ -45,19 +46,24 @@ public class CombatTask extends AbstractTask{
         System.out.printf("Top: %f\n", entity.getBounds().getTop());
         System.out.printf("Width: %d\n", GameManager.getManagerFromInstance(TextureManager.class).getTexture("projectile").getWidth());
         System.out.printf("Height: %d\n", GameManager.getManagerFromInstance(TextureManager.class).getTexture("projectile").getHeight());
+         */
 
         if (!collidingEntities.isEmpty()) {
             for (AbstractEntity e : collidingEntities) {
-                System.out.println(e.getObjectName());
-                if (e.getObjectName() == "playerPeon") {
-                    applyDamage(((PlayerPeon) e));
+                //System.out.println(e.getObjectName());
+                if (e.getObjectName() == "Orc") {
+                    applyDamage(((EnemyPeon) e));
                 }
             }
         }
     }
 
-    private void applyDamage (PlayerPeon e) {
+    private void applyDamage (EnemyPeon e) {
         e.reduceHealth(entity.getDamage());
-        System.out.println(e.getCurrentHealth());
+        this.taskComplete = true;
+        if (e.isDead()) {
+            e.death();
+
+        }
     }
 }
