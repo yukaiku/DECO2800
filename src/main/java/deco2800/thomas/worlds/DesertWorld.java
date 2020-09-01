38892f230e5f4e8b2deb861f7a9e7e44ca191e6b
Zachary Oar <z.oar@uqconnect.edu.au>
2020-09-01 22:27:52 +1000

    Added coloured tiles to generate the Desert World, included random static objects and dark patches

--- src/main/java/deco2800/thomas/worlds/DesertWorld.java
+++ src/main/java/deco2800/thomas/worlds/DesertWorld.java
@@ -1,15 +1,22 @@
 package deco2800.thomas.worlds;
 
+import deco2800.thomas.entities.AbstractEntity;
 import deco2800.thomas.entities.PlayerPeon;
+import deco2800.thomas.entities.Rock;
+import deco2800.thomas.entities.Tree;
+import deco2800.thomas.managers.GameManager;
 
 import java.util.Random;
 
 public class DesertWorld extends AbstractWorld {
 
+    private boolean notGenerated = true;
+
     /**
      * Constructor that creates a world with default width and height
      */
     public DesertWorld() {
+        super();
     }
 
     /**
@@ -27,19 +34,97 @@ public class DesertWorld extends AbstractWorld {
      */
     @Override
     protected void generateTiles() {
-        int index;
         Random rand = new Random();
-        String[] names = new String[]{"desert_1", "desert_2", "desert_3", "desert_4", "desert_5", "desert_6"};
 
+        // makes all tiles for the world
         for (int x = -width; x < width; x++) {
             for (int y = -height; y < height; y++) {
-                index = rand.nextInt(6);
-                tiles.add(new DesertTile(names[index], x, y));
+                // randomly assign a tile one of two colours
+                if (rand.nextInt(3) > 0) {
+                    tiles.add(new DesertTile("desert_1", x, y));
+                } else {
+                    tiles.add(new DesertTile("desert_2", x, y));
+                }
             }
         }
 
+        // Sets the top/bottom of the world to a darker tile shade
+        for (int x = -width; x < width; x++) {
+            getTile(x, -height).setTexture("desert_6");
+            getTile(x, height - 1).setTexture("desert_6");
 
-        // Create the entities in the game
+            // randomly sets half of the tiles to darker shades one off the top/bottom
+            if (rand.nextInt(2) == 0) {
+                getTile(x, -height + 1).setTexture("desert_6");
+            }
+            if (rand.nextInt(2) == 0) {
+                getTile(x, height - 2).setTexture("desert_6");
+            }
+        }
+
+        // Sets the left/right of the world to a darker tile shade
+        for (int y = -height; y < height; y++) {
+            getTile(-width, y).setTexture("desert_6");
+            getTile(width - 1, y).setTexture("desert_6");
+
+            // randomly sets half of the tiles to darker shades one off the left/right
+            if (rand.nextInt(2) == 0) {
+                getTile(-width + 1, y).setTexture("desert_6");
+            }
+            if (rand.nextInt(2) == 0) {
+                getTile(width - 2, y).setTexture("desert_6");
+            }
+        }
+
+        // Creates the player entity
         addEntity(new PlayerPeon(10f, 5f, 0.1f));
+    }
+
+
+    public void createStaticEntities() {
+        Random rand = new Random();
+        int tileCount = GameManager.get().getWorld().getTiles().size();
+
+        // Makes 20 patches of darker tiles randomly around the world
+        for (int i = 0; i < 20; i++) {
+            Tile tile = this.getTile(rand.nextInt(tileCount));
+            for (Tile neighbour : tile.getNeighbours().values()) {
+                for (Tile outer : neighbour.getNeighbours().values()) {
+                    outer.setTexture("desert_3");
+                }
+                neighbour.setTexture("desert_4");
+            }
+            tile.setTexture("desert_4");
+        }
+
+        // 100 Rocks randomly placed
+        for (int i = 0; i < 100; i++) {
+            Tile t = this.getTile(rand.nextInt(tileCount));
+            if (t != null) {
+                entities.add(new Rock(t, true));
+            }
+        }
+
+        // 50 Trees randomly placed
+        for (int i = 0; i < 50; i++) {
+            Tile t = this.getTile(rand.nextInt(tileCount));
+            if (t != null) {
+                entities.add(new Tree(t, true));
+            }
+        }
+    }
+
+    @Override
+    public void onTick(long i) {
+        super.onTick(i);
+
+        for (AbstractEntity e : this.getEntities()) {
+            e.onTick(0);
+        }
+
+        if (notGenerated) {
+            createStaticEntities();
+            notGenerated = false;
+        }
     }
 }
