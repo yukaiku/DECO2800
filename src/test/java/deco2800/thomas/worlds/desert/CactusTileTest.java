d386f9a284aaea42189ad2d04b4b1b84a90de35a
Zachary Oar <z.oar@uqconnect.edu.au>
2020-09-06 16:15:56 +1000

    Added tests for desert zone tile classes

--- src/test/java/deco2800/thomas/worlds/desert/CactusTileTest.java
+++ src/test/java/deco2800/thomas/worlds/desert/CactusTileTest.java
@@ -1,4 +1,62 @@
+package deco2800.thomas.worlds.desert;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CactusTileTest {
-  
+
+    CactusTile cactus1;
+    CactusTile cactus2;
+    CactusTile cactus3;
+
+
+    @Before
+    public void setUp() throws Exception {
+        cactus1 = new CactusTile();
+        cactus2 = new CactusTile("desert_1");
+        cactus3 = new CactusTile("desert_2", 1f, 2f);
+    }
+
+    @Test
+    public void getTypeNullConstructor() {
+        Assert.assertEquals("Cactus", cactus1.getType());
+    }
+
+    @Test
+    public void getTypeTexConstructor() {
+        Assert.assertEquals("Cactus", cactus2.getType());
+    }
+
+    @Test
+    public void getTypeTexRowColConstructor() {
+        Assert.assertEquals("Cactus", cactus3.getType());
+    }
+
+    @Test
+    public void getTextureNameNullConstructor() {
+        Assert.assertNull(cactus1.getTextureName());
+    }
+
+    @Test
+    public void getTextureNameTexConstructor() {
+        Assert.assertEquals("desert_1", cactus2.getTextureName());
+    }
+
+    @Test
+    public void getTextureNameTexRowColConstructor() {
+        Assert.assertEquals("desert_2", cactus3.getTextureName());
+    }
+
+    @Test
+    public void getCol() {
+        Assert.assertEquals(1f, cactus3.getCol(), 0);
+    }
+
+    @Test
+    public void getRow() {
+        Assert.assertEquals(2f, cactus3.getRow(), 0);
+    }
 }
--- src/test/java/deco2800/thomas/worlds/desert/QuicksandTileTest.java
+++ src/test/java/deco2800/thomas/worlds/desert/QuicksandTileTest.java
@@ -1,4 +1,60 @@
-import static org.junit.Assert.*;
+package deco2800.thomas.worlds.desert;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class QuicksandTileTest {
-  
+
+    QuicksandTile quicksand1;
+    QuicksandTile quicksand2;
+    QuicksandTile quicksand3;
+
+
+    @Before
+    public void setUp() throws Exception {
+        quicksand1 = new QuicksandTile();
+        quicksand2 = new QuicksandTile("desert_1");
+        quicksand3 = new QuicksandTile("desert_2", 1f, 2f);
+    }
+
+    @Test
+    public void getTypeNullConstructor() {
+        Assert.assertEquals("Quicksand", quicksand1.getType());
+    }
+
+    @Test
+    public void getTypeTexConstructor() {
+        Assert.assertEquals("Quicksand", quicksand2.getType());
+    }
+
+    @Test
+    public void getTypeTexRowColConstructor() {
+        Assert.assertEquals("Quicksand", quicksand3.getType());
+    }
+
+    @Test
+    public void getTextureNameNullConstructor() {
+        Assert.assertNull(quicksand1.getTextureName());
+    }
+
+    @Test
+    public void getTextureNameTexConstructor() {
+        Assert.assertEquals("desert_1", quicksand2.getTextureName());
+    }
+
+    @Test
+    public void getTextureNameTexRowColConstructor() {
+        Assert.assertEquals("desert_2", quicksand3.getTextureName());
+    }
+
+    @Test
+    public void getCol() {
+        Assert.assertEquals(1f, quicksand3.getCol(), 0);
+    }
+
+    @Test
+    public void getRow() {
+        Assert.assertEquals(2f, quicksand3.getRow(), 0);
+    }
 }
