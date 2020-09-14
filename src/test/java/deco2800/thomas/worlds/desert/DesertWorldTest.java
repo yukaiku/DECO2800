1447e05e61d163751e1a55a1a079ca8e554d5367
Zachary Oar <z.oar@uqconnect.edu.au>
2020-09-14 18:47:27 +1000

    Began testing on DesertWorld

---
+++ src/test/java/deco2800/thomas/worlds/desert/DesertWorldTest.java
@@ -0,0 +1,91 @@
+package deco2800.thomas.worlds.desert;
+
+import com.badlogic.gdx.graphics.Texture;
+import deco2800.thomas.managers.*;
+import deco2800.thomas.worlds.Tile;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(GameManager.class)
+public class DesertWorldTest {
+
+    private DesertWorld world;
+
+    @Before
+    public void setUp() throws Exception {
+        // set up mocks of all managers used during a world's creation
+        PowerMockito.mockStatic(GameManager.class);
+        GameManager gameManager = mock(GameManager.class);
+        EnemyManager enemyManager = mock(EnemyManager.class);
+        InputManager inputManager = mock(InputManager.class);
+        OnScreenMessageManager onScreenMessageManager = mock(OnScreenMessageManager.class);
+        TextureManager textureManager = mock(TextureManager.class);
+        when(GameManager.get()).thenReturn(gameManager);
+        when(gameManager.getManager(OnScreenMessageManager.class)).thenReturn(onScreenMessageManager);
+        when(gameManager.getManager(InputManager.class)).thenReturn(inputManager);
+        when(gameManager.getManager(EnemyManager.class)).thenReturn(enemyManager);
+        when(gameManager.getManager(TextureManager.class)).thenReturn(textureManager);
+        when(GameManager.getManagerFromInstance(OnScreenMessageManager.class)).thenReturn(onScreenMessageManager);
+        when(GameManager.getManagerFromInstance(InputManager.class)).thenReturn(inputManager);
+        when(GameManager.getManagerFromInstance(EnemyManager.class)).thenReturn(enemyManager);
+        when(GameManager.getManagerFromInstance(TextureManager.class)).thenReturn(textureManager);
+
+        Texture texture = mock(Texture.class);
+        when(textureManager.getTexture(anyString())).thenReturn(texture);
+        when(texture.getWidth()).thenReturn(1);
+        when(texture.getHeight()).thenReturn(1);
+
+        world = new DesertWorld();
+    }
+
+    @Test
+    public void getType() {
+        Assert.assertEquals("Desert", world.getType());
+    }
+
+    @Test
+    public void getTilesLength() {
+        Assert.assertEquals(2500, world.getTiles().size());
+    }
+
+    @Test
+    public void tilesNotNull() {
+        boolean allTiles = true;
+        for (Tile tile : world.getTiles()) {
+            if (tile == null) {
+                allTiles = false;
+                break;
+            }
+        }
+
+        Assert.assertTrue(allTiles);
+    }
+
+    @Test
+    public void tileIDsMatch() {
+        boolean allTiles = true;
+        int i = 0;
+        for (Tile tile : world.getTiles()) {
+            if (tile.getTileID() != i++) {
+                System.out.print(i);
+                System.out.print(" ");
+                System.out.println(tile.getTileID());
+                allTiles = false;
+                break;
+            }
+        }
+
+        Assert.assertTrue(allTiles);
+    }
+
+
+}
