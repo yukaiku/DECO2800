2a8c986a5680f063469d5a89896ce6ee9c39ac20
Chris Barnett <c.barnett@uqconnect.edu.au>
2020-09-15 17:51:15 +1000

    Added DamgeType enum.

---
+++ src/main/java/deco2800/thomas/combat/DamageType.java
@@ -0,0 +1,11 @@
+package deco2800.thomas.combat;
+
+/**
+ * Damage types that peons can attack with, and be vulnerable to.
+ */
+public enum DamageType {
+    FIRE,
+    SWAMPY_WATER,
+    SAND_I_GUESS,
+    ICE
+}
