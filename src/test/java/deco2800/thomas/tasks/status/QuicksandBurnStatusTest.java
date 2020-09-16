91a4b16b30a071bfebfe7485ff21d201e339e81c
Zachary Oar <z.oar@uqconnect.edu.au>
2020-09-16 22:06:28 +1000

    Finished tests for the speed status

---
+++ src/test/java/deco2800/thomas/tasks/status/QuicksandBurnStatusTest.java
@@ -0,0 +1,17 @@
+package deco2800.thomas.tasks.status;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuicksandBurnStatusTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void applyEffect() {
+    }
+}
