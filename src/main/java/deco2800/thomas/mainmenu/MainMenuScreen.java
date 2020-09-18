531d788468519a9583ca350d13be564eab21c7cd
Yiyun <s4513350@student.uq.edu.au>
2020-09-18 23:35:59 +1000

    Resized the fireball; Waiting for a proper background

--- src/main/java/deco2800/thomas/mainmenu/MainMenuScreen.java
+++ src/main/java/deco2800/thomas/mainmenu/MainMenuScreen.java
@@ -3,7 +3,9 @@ package deco2800.thomas.mainmenu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
@@ -35,19 +37,27 @@ public class MainMenuScreen implements Screen {
 
 		Label logo = new Label("Polyhedron", skin);
 		logo.setFontScale(5.0f);
-		logo.setPosition(1280 / 2 - 225, 720 / 2 + 100);
+//		logo.setPosition(1280 / 2 - 225, 720 / 2 + 100);
+		logo.setPosition(180, 510);
+		logo.addAction(Actions.moveTo(280, 510, 0.4f, Interpolation.PowOut.pow3Out));
 		stage.addActor(logo);
 
 		Button envTeamButton = new TextButton("ENV TEAM", skin, "main_menu");
-		envTeamButton.setPosition(10, 250);
+//		envTeamButton.setPosition(10, 250);
+		envTeamButton.setPosition(200, 300);
+		envTeamButton.addAction(Actions.moveTo(300, 300, 0.6f, Interpolation.PowOut.pow3Out));
 		stage.addActor(envTeamButton);
 
 		Button newGameBtn = new TextButton("SINGLE PLAYER", skin, "main_menu");
-		newGameBtn.setPosition(10, 200);
+//		newGameBtn.setPosition(10, 200);
+		newGameBtn.setPosition(200, 250);
+		newGameBtn.addAction(Actions.moveTo(300, 250, 0.8f, Interpolation.PowOut.pow3Out));
 		stage.addActor(newGameBtn);
 
 		Button testWorldBtn = new TextButton("TEST WORLD", skin, "main_menu");
-		testWorldBtn.setPosition(10, 150);
+//		testWorldBtn.setPosition(10, 150);
+		testWorldBtn.setPosition(200, 200);
+		testWorldBtn.addAction(Actions.moveTo(300, 200, 1f, Interpolation.PowOut.pow3Out));
 		stage.addActor(testWorldBtn);
 
 		newGameBtn.addListener(new ClickListener() {
@@ -117,6 +127,9 @@ public class MainMenuScreen implements Screen {
 	 */
 	public void render(float delta) {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.getRoot().getColor().a = 0;
+		stage.getRoot().addAction(Actions.fadeIn(1f));
 
 		stage.act(delta);
 		stage.draw();
