60545dab3bf0e5010619e99b28d5ec3f4332eb34
Arshia <s4482296@uq.edu.au>
2020-09-19 22:41:43 +1000

    Items no longer obstructed, changed interact bounds.

--- src/main/java/deco2800/thomas/entities/items/Item.java
+++ src/main/java/deco2800/thomas/entities/items/Item.java
@@ -76,24 +76,12 @@ public class Item extends StaticEntity implements TouchDownObserver {
             player.takeMoney(goldValue);
         }
     }
-
-    public void setObstructions() {
-        try {
-            Tile feet = GameManager.get().getWorld().getTile(this.getCol(),
-                    this.getRow());
-            feet.setObstructed(true);
-            GameManager.get().getWorld().updateTile(feet);
-            ready = true;
-            GameManager.getManagerFromInstance(InputManager.class).addTouchDownListener(this);
-        } catch (NullPointerException e) {
-            // do nothing, it'll work eventually.
-        }
-    }
     
     @Override
     public void onTick(long i) {
         if (GameManager.get().getWorld() != null && !ready) {
-            setObstructions();
+            ready = true;
+            GameManager.getManagerFromInstance(InputManager.class).addTouchDownListener(this);
         }
         if (this.display.isShowing()) {
             this.display.setVisibleTime(display.getVisibleTime() + 1);
@@ -107,18 +95,14 @@ public class Item extends StaticEntity implements TouchDownObserver {
 
     @Override
     public void notifyTouchDown(int screenX, int screenY, int pointer, int button) {
-        
         float[] mouse = WorldUtil.screenToWorldCoordinates(Gdx.input.getX(),
                 Gdx.input.getY());
         float[] clickedPosition = WorldUtil.worldCoordinatesToColRow(mouse[0], mouse[1]);
-
         float playerCol = this.player.getPosition().getCol();
         float playerRow = this.player.getPosition().getRow();
-        
-
         float itemCol = this.getPosition().getCol();
         float itemRow = this.getPosition().getRow();
-
+        
         boolean isCloseCol =
                 (clickedPosition[0] == this.getCol()
                         || clickedPosition[0] == this.getRow() - 1
@@ -127,37 +111,20 @@ public class Item extends StaticEntity implements TouchDownObserver {
         boolean isCloseRow = (clickedPosition[1] == this.getRow() ||
                 clickedPosition[1] == this.getRow() - 1 ||
                 clickedPosition[1] == this.getRow() + 1);
-
-        // if click is close enough, then player should also be somewhat close.
+        
         if (isCloseCol && isCloseRow) {
-
-            // less than or just one column above.
-            // less than or just one row above.
-            if (((itemCol + 2) <= playerCol) && ((itemRow+2) <= playerRow)) {
-                System.out.println("Interacting 1!");
+            if (((itemCol + 1) <= playerCol) && ((itemRow+1) <= playerRow)) {
                 interact();
             }
-            // less than / 1 column below
-            // less than / 1 row below.
-            if ((itemCol - 2) <= playerCol && ((itemRow -2) <= playerRow)) {
-                System.out.println("Interacting 2!");
+            if ((itemCol - 1) <= playerCol && ((itemRow -1) <= playerRow)) {
                 interact();
             }
-
-            // less/than 1 column above
-            // lessthan/1 row below
-            if ((itemCol - 2) <= playerCol && ((itemRow + 2) <= playerRow)) {
-                System.out.println("Interacting 2!");
+            if ((itemCol - 1) <= playerCol && ((itemRow + 1) <= playerRow)) {
                 interact();
             }
-
-            // lessthan/1 column below
-            // less than/1 row above.
-            if ((itemCol + 2) <= playerCol && ((itemRow - 2) <= playerRow)) {
-                System.out.println("Interacting 2!");
+            if ((itemCol + 1) <= playerCol && ((itemRow - 1) <= playerRow)) {
                 interact();
             }
         }
     }
-    
 }
