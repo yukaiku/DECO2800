b9e58ad95328038aab9b52e3bf43473816e499d8
Arshia <s4482296@uq.edu.au>
2020-09-20 13:44:37 +1000

    DialogManager unit tests

--- src/test/java/deco2800/thomas/managers/DialogManagerTest.java
+++ src/test/java/deco2800/thomas/managers/DialogManagerTest.java
@@ -1,16 +1,35 @@
 package deco2800.thomas.managers;
 
+import com.badlogic.gdx.physics.box2d.World;
 import deco2800.thomas.BaseGDXTest;
+import deco2800.thomas.entities.AbstractDialogBox;
+import deco2800.thomas.entities.ItemBox;
+import deco2800.thomas.entities.agent.PlayerPeon;
+import deco2800.thomas.entities.items.Item;
+import deco2800.thomas.entities.items.Treasure;
+import deco2800.thomas.worlds.TestWorld;
+import deco2800.thomas.worlds.Tile;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
 
 public class DialogManagerTest extends BaseGDXTest {
-	
-	// add some items 
-	// once opened 
-	// change viisble time - make sure closed. 
-	
-	// set remove, make sure it doesn't exist anymore 
-	
-	// try to show both of them 
-	// make sure only 1 showing. 
-	
+
+	@Test
+	public void checkConstructor() {
+		ArrayList<AbstractDialogBox> boxes = new ArrayList<>();
+		TestWorld world = new TestWorld();
+		Tile tile = new Tile("treasure-box");
+		PlayerPeon player = mock(PlayerPeon.class);
+		Item item = new Treasure(tile, false, player, "volcano");
+		ItemBox tester = new ItemBox(item, "Treasure Box"
+				, "0", "some description", "volcano");
+
+		// ready is false since game manager is null. 
+		DialogManager dialog = new DialogManager(world, player, boxes);
+		Assert.assertFalse(dialog.getReady());
+	}
 }
