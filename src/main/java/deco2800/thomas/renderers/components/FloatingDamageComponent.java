83c920a18290f5249c8d1207aaeaf50c280e3a8b
Chris Barnett <c.barnett@uqconnect.edu.au>
2020-09-27 11:50:18 +1000

    Implemented FloatingDamageText.

---
+++ src/main/java/deco2800/thomas/renderers/components/FloatingDamageComponent.java
@@ -0,0 +1,9 @@
+package deco2800.thomas.renderers.components;
+
+/**
+ * This component renders floating damage numbers above the heads of
+ * entities that have been hit by damage, or received a heal.
+ */
+public class FloatingDamageComponent {
+
+}
