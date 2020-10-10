8488e947a0695c5658e0beb3fb4a52280694cd61
ArthurM99115 <arthur.mitchell4@gmail.com>
2020-10-10 20:59:45 +1000

    Docstrings done, initial tests done, Textures todo

--- src/main/java/deco2800/thomas/managers/GameManager.java
+++ src/main/java/deco2800/thomas/managers/GameManager.java
@@ -368,6 +368,7 @@ public class GameManager {
 		//Add existing world & enemy manager
 		this.addManager(enemyManagerOutSideWorld);
 		this.setWorld(worldOutsideDungeon);
+		((PlayerPeon)this.worldOutsideDungeon.getPlayerEntity()).updatePlayerSkills();
 		this.worldOutsideDungeon = null;
 	}
 
