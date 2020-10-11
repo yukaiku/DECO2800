8a91ab4507b8622d0f8d4824cf42f671984fb942
finn-cooney <s4482633@student.uq.edu.au>
2020-10-11 16:11:51 +1000

    Added TornadoOnCollision task test

--- src/test/java/deco2800/thomas/tasks/combat/ApplyTornadoOnCollisionTaskTest.java
+++ src/test/java/deco2800/thomas/tasks/combat/ApplyTornadoOnCollisionTaskTest.java
@@ -1,10 +1,13 @@
 package deco2800.thomas.tasks.combat;
 
+import deco2800.thomas.BaseGDXTest;
+import deco2800.thomas.combat.DamageType;
 import deco2800.thomas.entities.EntityFaction;
 import deco2800.thomas.entities.agent.Peon;
+import deco2800.thomas.entities.agent.PlayerPeon;
 import deco2800.thomas.entities.attacks.CombatEntity;
 import deco2800.thomas.managers.GameManager;
-import deco2800.thomas.tasks.status.SpeedStatus;
+import deco2800.thomas.tasks.status.TornadoStatus;
 import deco2800.thomas.util.BoundingBox;
 import deco2800.thomas.util.SquareVector;
 import deco2800.thomas.worlds.AbstractWorld;
@@ -19,18 +22,19 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(GameManager.class)
-public class ApplySlowOnCollisionTaskTest {
-    // Used to verify task execution
+public class ApplyTornadoOnCollisionTaskTest extends BaseGDXTest {
+
     private GameManager gameManager;
     private CombatEntity combatEntity;
     private Peon peon;
     private AbstractWorld abstractWorld;
-    private ApplySlowOnCollisionTask task;
+    private ApplyTornadoOnCollisionTask task;
 
     @Before
     public void setUp() throws Exception {
@@ -43,7 +47,7 @@ public class ApplySlowOnCollisionTaskTest {
         combatEntity = mock(CombatEntity.class);
         when(combatEntity.getFaction()).thenReturn(EntityFaction.EVIL);
         when(combatEntity.getBounds()).thenReturn(new BoundingBox(new SquareVector(0, 0), 10, 10));
-        peon = mock(Peon.class);
+        peon = mock(PlayerPeon.class);
         when(peon.getFaction()).thenReturn(EntityFaction.ALLY);
 
         // Mock abstract world
@@ -53,22 +57,29 @@ public class ApplySlowOnCollisionTaskTest {
                 .thenReturn(new ArrayList<>(Arrays.asList(combatEntity, peon)));
 
         // Start task
-        task = new ApplySlowOnCollisionTask(combatEntity, 10, 0.1f, 5);
+        task = new ApplyTornadoOnCollisionTask(combatEntity, 1);
     }
 
-    /**
-     * Verifies that the applyDamage method is called when the entities
-     * collide. A passing test is when the reduceHealth and getDamage methods are called
-     * from the player peon and fireball respectively.
-     */
     @Test
-    public void applyingSlowOnCollisionTest() {
+    public void applyingTornadoOnCollisionTest() {
         task.onTick(1);
 
+        // Verify getDamage and reduceHealth are called
+        verify(combatEntity).getDamage();
+        verify(peon).applyDamage(anyInt(), any(DamageType.class));
+
         // Verify addEffect is called
-        verify(peon).addEffect(any(SpeedStatus.class));
+        verify(peon).addEffect(any(TornadoStatus.class));
 
         // Verify state change
         assertTrue(task.isComplete());
+    }
+
+    @Test
+    public void isAlive() {
+    }
+
+    @Test
+    public void onTick() {
     }
 }
