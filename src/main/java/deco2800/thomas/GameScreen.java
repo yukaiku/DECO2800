85e348e7ddc383ba7f1d5bb843c7ec01e9da7691
yukaiku <yukai.ku@uq.net.au>
2020-10-12 14:44:26 +1000

    resolved merge issue, with missing skip world function

--- src/main/java/deco2800/thomas/GameScreen.java
+++ src/main/java/deco2800/thomas/GameScreen.java
@@ -14,9 +14,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import deco2800.thomas.combat.DamageType;
 import deco2800.thomas.entities.AbstractEntity;
 import deco2800.thomas.entities.agent.Peon;
+import deco2800.thomas.entities.agent.PlayerPeon;
 import deco2800.thomas.entities.agent.QuestTracker;
+import deco2800.thomas.entities.enemies.bosses.Boss;
 import deco2800.thomas.handlers.KeyboardManager;
 import deco2800.thomas.managers.*;
 import deco2800.thomas.observers.KeyDownObserver;
@@ -411,7 +414,15 @@ public class GameScreen implements Screen, KeyDownObserver {
 		if (keycode == Input.Keys.F12 && GameManager.get().state == GameManager.State.RUN) {
 			GameManager.get().debugMode = !GameManager.get().debugMode;
 		}
-
+		if (GameManager.get().debugMode && !GameManager.get().getWorld().getType().equals("World") && (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) ||
+				Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT))) {
+			if (keycode == Input.Keys.N) {
+				Boss boss = GameManager.getManagerFromInstance(EnemyManager.class).getBoss();
+				PlayerPeon playerPeon = (PlayerPeon) GameManager.get().getWorld().getPlayerEntity();
+				playerPeon.setPosition(boss.getPosition().getCol(),boss.getPosition().getRow(),boss.getHeight());
+				boss.applyDamage(boss.getCurrentHealth(), DamageType.COMMON);
+			}
+		}
 		if (keycode == Input.Keys.ESCAPE) {
 			if (GameManager.get().state == GameManager.State.RUN) {
 				GameManager.pause();
