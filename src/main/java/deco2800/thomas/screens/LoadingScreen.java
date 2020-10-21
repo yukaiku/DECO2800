821c605978a6132e5872ca732efdb94c76a49852
Yiyun <s4513350@student.uq.edu.au>
2020-10-21 19:54:43 +1000

    Add loading screen status, distribute loadings of the textures

--- src/main/java/deco2800/thomas/screens/LoadingScreen.java
+++ src/main/java/deco2800/thomas/screens/LoadingScreen.java
@@ -1,6 +1,5 @@
 package deco2800.thomas.screens;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
@@ -12,52 +11,44 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import deco2800.thomas.ThomasGame;
 import deco2800.thomas.managers.GameManager;
 import deco2800.thomas.managers.TextureManager;
-import org.lwjgl.Sys;
-
-import java.util.logging.ConsoleHandler;
 
 public class LoadingScreen implements Screen {
 
-    private static LoadingScreen instance = null;
-
     final ThomasGame game;
-    private Stage stage;
-    private Label loadingTitle;
-    private Label loadingText;
+    private final Stage stage;
+    private final Label loadingText;
 
     public LoadingScreen(ThomasGame game) {
         this.game = game;
         stage = new Stage(new ExtendViewport(1920, 1080), game.getBatch());
 
-        Texture menuTex = new Texture(Gdx.files.internal("resources/fonts/title.png"), true);
-        menuTex.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear);
-        BitmapFont menuFont = new BitmapFont(Gdx.files.internal("resources/fonts/title.fnt"),
-                new TextureRegion(menuTex), false);
-        Label.LabelStyle menuStyle = new Label.LabelStyle();
-        menuStyle.font = menuFont;
-        menuStyle.fontColor = Color.valueOf("#cccccc");
-
-        Texture menuTex2 = new Texture(Gdx.files.internal("resources/fonts/times.png"), true);
-        menuTex2.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear);
-        BitmapFont menuFont2 = new BitmapFont(Gdx.files.internal("resources/fonts/times.fnt"),
-                new TextureRegion(menuTex2), false);
-        Label.LabelStyle menuStyle2 = new Label.LabelStyle();
-        menuStyle2.font = menuFont2;
-        menuStyle2.fontColor = Color.valueOf("#cccccc");
-
-        loadingTitle = new Label("LOADING...", menuStyle);
+        Texture titleTex = new Texture(Gdx.files.internal("resources/fonts/title.png"), true);
+        titleTex.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear);
+        BitmapFont titleFont = new BitmapFont(Gdx.files.internal("resources/fonts/title.fnt"),
+                new TextureRegion(titleTex), false);
+        Label.LabelStyle titleStyle = new Label.LabelStyle();
+        titleStyle.font = titleFont;
+        titleStyle.fontColor = Color.valueOf("#cccccc");
+        Label loadingTitle = new Label("LOADING...", titleStyle);
         loadingTitle.setFontScale(1.3f);
         loadingTitle.setPosition(120, 140);
-        stage.addActor(loadingTitle);
 
-        loadingText = new Label("", menuStyle2);
+        Texture textTex = new Texture(Gdx.files.internal("resources/fonts/times.png"), true);
+        textTex.setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Linear);
+        BitmapFont textFont = new BitmapFont(Gdx.files.internal("resources/fonts/times.fnt"),
+                new TextureRegion(textTex), false);
+        Label.LabelStyle textStyle = new Label.LabelStyle();
+        textStyle.font = textFont;
+        textStyle.fontColor = Color.valueOf("#cccccc");
+        loadingText = new Label("", textStyle);
         loadingText.setFontScale(0.9f);
         loadingText.setPosition(122, 120);
+
+        stage.addActor(loadingTitle);
         stage.addActor(loadingText);
     }
 
@@ -69,10 +60,18 @@ public class LoadingScreen implements Screen {
     public void show() {
         stage.getRoot().getColor().a = 0;
         SequenceAction sequenceAction = new SequenceAction();
-        sequenceAction.addAction(Actions.fadeIn(0.4f));
+        sequenceAction.addAction(Actions.fadeIn(0.3f));
         sequenceAction.addAction(Actions.run(() -> {
+            setLoadingText("Loading textures");
             game.loadMainMenuScreen();
-
+        }));
+        sequenceAction.addAction(Actions.run(() -> {
+            setLoadingText("Loading enemies");
+            GameManager.get().getManager(TextureManager.class).addEnemyTextures();
+        }));
+        sequenceAction.addAction(Actions.run(() -> {
+            setLoadingText("Loading health");
+            GameManager.get().getManager(TextureManager.class).addHealthTextures();
         }));
         sequenceAction.addAction(Actions.run(() -> {
             setLoadingText("Loading environment");
@@ -81,13 +80,10 @@ public class LoadingScreen implements Screen {
         sequenceAction.addAction(Actions.run(() -> {
             setLoadingText("Loading animation");
             GameManager.get().getManager(TextureManager.class).addAnimationFrames();
-//            stage.getRoot().getColor().a = 0;
         }));
-        sequenceAction.addAction(Actions.fadeOut(0.5f));
-        sequenceAction.addAction(Actions.run(() -> {
-//            stage.getRoot().getColor().a = 1;
-           game.setMainMenuScreen();
-        }));
+        sequenceAction.addAction(Actions.run(() -> setLoadingText("")));
+        sequenceAction.addAction(Actions.fadeOut(0.2f));
+        sequenceAction.addAction(Actions.run(game::setMainMenuScreen));
         stage.getRoot().addAction(sequenceAction);
     }
 
