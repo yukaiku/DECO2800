bd973ca6a2605ec0d0d23904dd1cd943b39c3e91
Wilson <s4528577@student.uq.edu.au>
2020-10-24 23:45:39 +1000

    Add enemyUtilTest

---
+++ src/test/java/deco2800/thomas/entities/enemies/EnemyUtilTest.java
@@ -0,0 +1,10 @@
+package deco2800.thomas.entities.enemies;
+
+import deco2800.thomas.BaseGDXTest;
+import deco2800.thomas.entities.agent.PlayerPeon;
+import deco2800.thomas.entities.enemies.bosses.VolcanoDragon;
+
+public class EnemyUtilTest extends BaseGDXTest {
+    private PlayerPeon playerPeon;
+    private VolcanoDragon volcanoDragon;
+}
