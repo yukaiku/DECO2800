3d517ab3d2c202e829a44a2597102b841d4cc330
Chris Barnett <c.barnett@uqconnect.edu.au>
2020-10-25 12:54:01 +1000

    Implemented tests for Explosion and CombatEntity classes.

--- src/test/java/deco2800/thomas/entities/attacks/ExplosionTest.java
+++ src/test/java/deco2800/thomas/entities/attacks/ExplosionTest.java
@@ -11,12 +11,14 @@ import deco2800.thomas.managers.TextureManager;
 import deco2800.thomas.tasks.AbstractTask;
 
 import deco2800.thomas.util.WorldUtil;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
@@ -28,6 +30,27 @@ import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({GameManager.class, WorldUtil.class})
 public class ExplosionTest extends BaseGDXTest {
+    @Before
+    public void setup() {
+        // Mock a texture manager, a texture and an animation
+        Texture texture = mock(Texture.class);
+        when(texture.getWidth()).thenReturn(10);
+        when(texture.getHeight()).thenReturn(10);
+        Array<TextureRegion> animation = new Array<>();
+        animation.add(new TextureRegion(new Texture("resources/combat/move_right.png"), 262, 256));
+
+        TextureManager textureManager = mock(TextureManager.class);
+        when(textureManager.getTexture(anyString())).thenReturn(texture);
+        when(textureManager.getAnimationFrames(anyString())).thenReturn(animation);
+
+        // Mock the game manager, and a CombatManager
+        GameManager gameManager = mock(GameManager.class);
+        mockStatic(WorldUtil.class);
+        mockStatic(GameManager.class);
+        when(GameManager.get()).thenReturn(gameManager);
+        when(GameManager.getManagerFromInstance(TextureManager.class)).thenReturn(textureManager);
+    }
+
     /**
      * Tests the constructor creates a valid instance from arguments.
      */
@@ -69,24 +92,6 @@ public class ExplosionTest extends BaseGDXTest {
         AbstractTask combatTask = mock(AbstractTask.class);
         when(combatTask.isComplete()).thenReturn(true);
 
-        // Mock a texture manager, a texture and an animation
-        Texture texture = mock(Texture.class);
-        when(texture.getWidth()).thenReturn(10);
-        when(texture.getHeight()).thenReturn(10);
-        Array<TextureRegion> animation = new Array<>();
-        animation.add(new TextureRegion(new Texture("resources/combat/move_right.png"), 262, 256));
-
-        TextureManager textureManager = mock(TextureManager.class);
-        when(textureManager.getTexture(anyString())).thenReturn(texture);
-        when(textureManager.getAnimationFrames(anyString())).thenReturn(animation);
-
-        // Mock the game manager, and a CombatManager
-        GameManager gameManager = mock(GameManager.class);
-        mockStatic(WorldUtil.class);
-        mockStatic(GameManager.class);
-        when(GameManager.get()).thenReturn(gameManager);
-        when(GameManager.getManagerFromInstance(TextureManager.class)).thenReturn(textureManager);
-
         // Create explosion to test on, and set tasks
         Explosion explosion = new Explosion(0, 0, 10, EntityFaction.ALLY);
         explosion.setCombatTask(combatTask);
@@ -95,5 +100,34 @@ public class ExplosionTest extends BaseGDXTest {
         explosion.onTick(0);
         verifyStatic(WorldUtil.class);
         WorldUtil.removeEntity(explosion);
+    }
+
+    /**
+     * Tests that the entity death occurs when no combat task is set.
+     */
+    @Test
+    public void testProjectileDeath2() {
+        // Create explosion to test on
+        Explosion explosion = new Explosion(0, 0, 10, EntityFaction.ALLY);
+
+        // Call on tick, then verify that the entity was remove from the world
+        explosion.onTick(0);
+        verifyStatic(WorldUtil.class);
+        WorldUtil.removeEntity(explosion);
+    }
+
+    /**
+     * Tests that getFrame returns valid texture regions.
+     */
+    @Test
+    public void testGetFrame() {
+        // Create explosion to test on
+        Explosion explosion = new Explosion(0, 0, 10, EntityFaction.ALLY);
+
+        // Run animation
+        assertNotNull(explosion.getFrame(0.1f));
+        assertNotNull(explosion.getFrame(1f));
+        assertNotNull(explosion.getFrame(10f));
+        assertNotNull(explosion.getFrame(100f));
     }
 }
