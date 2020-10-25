1afa80fbaea1d365411468d992095d418ed99511
yukaiku <yukai.ku@uq.net.au>
2020-10-25 12:50:57 +1000

    optimise imports

--- src/main/java/deco2800/thomas/screens/GameScreen.java
+++ src/main/java/deco2800/thomas/screens/GameScreen.java
@@ -30,7 +30,6 @@ import deco2800.thomas.worlds.AbstractWorld;
 import deco2800.thomas.worlds.TestWorld;
 import deco2800.thomas.worlds.Tile;
 import deco2800.thomas.worlds.TutorialWorld;
-import deco2800.thomas.worlds.desert.DesertWorld;
 import deco2800.thomas.worlds.volcano.VolcanoWorld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
